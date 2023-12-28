5e2e0f9684436b7063d858db2e40e046b5287971
ZhenyaKasatkina <knopisha.zh@gmail.com>
2023-12-28 19:39:49 +0300

    change src/item.py, tests/test_item.py, pyproject.toml (согласно файла homework-1/README.md)

--- src/item.py
+++ src/item.py
@@ -13,18 +13,22 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        pass
+        total_cost = self.price * self.quantity
+        return total_cost
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
