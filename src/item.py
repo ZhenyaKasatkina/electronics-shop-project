20e69d58ba1ab4d2298f1446e15d5ad677abf197
ZhenyaKasatkina <knopisha.zh@gmail.com>
2023-12-30 15:39:33 +0300

    change src/item.py, tests/test_item.py, .gitignore (согласно файла homework-1/README.md) add config.py, tests/test_items.csv

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+import os
+from config import ROOT_DIR
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +18,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
-        self.all.append(self)
+        # self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +37,36 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_item):
+        """Проверяет, что длина наименования товара не больше 10 символов.
+        В противном случае, обрезать строку (оставив первые 10 символов)"""
+
+        if len(name_item) <= 10:
+            self.__name = name_item
+        else:
+            self.__name = "".join(list(name_item)[:10])
+
+    @classmethod
+    def instantiate_from_csv(cls, file_csv):
+        """Класс-метод, инициализирующий экземпляры класса `Item`
+        данными из файла _src/items.csv_"""
+        cls.all.clear()
+        with (open(os.path.join(ROOT_DIR, file_csv), newline='', encoding='cp1251') as csvfile):
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls.all.append(cls(name, price, quantity))
+            return cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(item_str):
+        """Статический метод, возвращающий число из числа-строки"""
+        return int(item_str.split(".")[0])
